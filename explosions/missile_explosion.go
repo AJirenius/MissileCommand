26d1e7c6ee47c6f7b2116adb9ac490d3717e6c3c
Andreas Jirenius <andreas.jirenius@king.com>
2015-10-18 15:53:36 +0200

    first commit

---
+++ explosions/missile_explosion.go
@@ -0,0 +1,30 @@
+components {
+  id: "fx"
+  component: "/game/gfx/fx/missile_explosion.particlefx"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "script"
+  component: "/explosions/missile_explosion.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
---
+++ game/shared/bezier.lua
@@ -0,0 +1,13 @@
+local M = {}
+function M.bezier4(p1,p2,p3,p4,mu)
+   local mum1,mum13,mu3;
+   local p = {}
+   mum1 = 1 - mu
+   mum13 = mum1 * mum1 * mum1
+   mu3 = mu * mu * mu
+   p.x = mum13*p1.x + 3*mu*mum1*mum1*p2.x + 3*mu*mu*mum1*p3.x + mu3*p4.x
+   p.y = mum13*p1.y + 3*mu*mum1*mum1*p2.y + 3*mu*mu*mum1*p3.y + mu3*p4.y
+   return p	
+end
+
+return M
